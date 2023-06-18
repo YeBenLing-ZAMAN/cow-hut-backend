1e4f5c20a95804ab81d2b7ccee3215d091a5785a
md kamruzzaman <kamruzzamanmd@hotmail.com>
2023-06-18 19:12:53 +0600

    bulid-update  again

--- dist/app/modules/cow/cow.constants.js
+++ dist/app/modules/cow/cow.constants.js
@@ -1,39 +1,31 @@
-'use strict'
-Object.defineProperty(exports, '__esModule', { value: true })
-exports.CowFilterAbleFields =
-  exports.CowSearchAbleFields =
-  exports.category =
-  exports.label =
-  exports.breed =
-  exports.location =
-    void 0
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CowFilterAbleFields = exports.CowSearchAbleFields = exports.category = exports.label = exports.breed = exports.location = void 0;
 exports.location = [
-  'Dhaka',
-  'Chattogram',
-  'Barishal',
-  'Rajshahi',
-  'Sylhet',
-  'Comilla',
-  'Rangpur',
-  'Mymensingh',
-]
+    'Dhaka',
+    'Chattogram',
+    'Barishal',
+    'Rajshahi',
+    'Sylhet',
+    'Comilla',
+    'Rangpur',
+    'Mymensingh',
+];
 exports.breed = [
-  'Brahman',
-  'Nellore',
-  'Sahiwal',
-  'Gir',
-  'Indigenous',
-  'Tharparkar',
-  'Kankrej',
-]
-exports.label = ['for sale', 'sold out']
-exports.category = ['Dairy', 'Beef', 'DualPurpose']
-exports.CowSearchAbleFields = [
-  'name',
-  'breed',
-  'category',
-  'location',
-  // 'price',
-  // 'weight',
-]
-exports.CowFilterAbleFields = ['searchTerm', 'minPrice', 'maxPrice', 'location']
+    'Brahman',
+    'Nellore',
+    'Sahiwal',
+    'Gir',
+    'Indigenous',
+    'Tharparkar',
+    'Kankrej',
+];
+exports.label = ['for sale', 'sold out'];
+exports.category = ['Dairy', 'Beef', 'DualPurpose'];
+exports.CowSearchAbleFields = ['name', 'breed', 'category', 'location'];
+exports.CowFilterAbleFields = [
+    'searchTerm',
+    'minPrice',
+    'maxPrice',
+    'location',
+];
