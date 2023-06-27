4945e131ccedb89a9cefaadc71d5e30d04a7d309
md kamruzzaman <kamruzzamanmd@hotmail.com>
2023-06-27 15:23:04 +0600

    [done] admin, buyer, seller authorized in cow  routes

--- src/app/modules/cow/cow.routes.ts
+++ src/app/modules/cow/cow.routes.ts
@@ -2,19 +2,31 @@ import express from 'express'
 import { CowController } from './cow.controllers'
 import { CowValidation } from './cow.validation'
 import { requestValidation } from '../../middleware/validationRequest'
+import auth from '../../middleware/auth'
+import { ENUM_USER_ROLE } from '../../enums/user'
 const router = express.Router()
 
 router.post(
   '/cow-create',
+  auth(ENUM_USER_ROLE.SELLER),
   requestValidation.validateRequest(CowValidation.createCowZodSchema),
   CowController.createCow
 )
 
-router.get('/', CowController.getAllCows)
-router.get('/:id', CowController.getSingleCow)
-router.delete('/:id', CowController.deleteCow)
+router.get(
+  '/',
+  auth(ENUM_USER_ROLE.SELLER, ENUM_USER_ROLE.BUYER, ENUM_USER_ROLE.ADMIN),
+  CowController.getAllCows
+)
+router.get(
+  '/:id',
+  auth(ENUM_USER_ROLE.SELLER, ENUM_USER_ROLE.BUYER, ENUM_USER_ROLE.ADMIN),
+  CowController.getSingleCow
+)
+router.delete('/:id', auth(ENUM_USER_ROLE.SELLER), CowController.deleteCow)
 router.patch(
   '/:id',
+  auth(ENUM_USER_ROLE.SELLER),
   requestValidation.validateRequest(CowValidation.updateCowZodSchema),
   CowController.updateCow
 )
