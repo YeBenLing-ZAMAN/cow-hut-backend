b9ec1eca93559a846b840580eee29ac09003dd46
md kamruzzaman <kamruzzamanmd@hotmail.com>
2023-06-27 17:55:59 +0600

    [extra code remove] my profile get and update by buyer and seller

--- src/app/middleware/auth.ts
+++ src/app/middleware/auth.ts
@@ -21,8 +21,7 @@ const auth =
       req.user = verifiedUser // role , userid
 
       // check by role // authenticated user
-      console.log(req.user)
-      console.log(verifiedUser.role)
+
       if (requiredRoles.length && !requiredRoles.includes(verifiedUser.role)) {
         throw new ApiError(httpStatus.FORBIDDEN, 'Forbidden request !')
       }
