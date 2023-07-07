b8e1d323692ef71c7422b5afce385504808ce2b5
md kamruzzaman <kamruzzamanmd@hotmail.com>
2023-07-07 15:50:26 +0600

    [update] router endpoint

--- dist/app/modules/admin/admin.controller.js
+++ dist/app/modules/admin/admin.controller.js
@@ -1,87 +1,135 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+'use strict'
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value)
+          })
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
+      function fulfilled(value) {
+        try {
+          step(generator.next(value))
+        } catch (e) {
+          reject(e)
         }
-    return t;
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AdminController = void 0;
-const catchAsync_1 = __importDefault(require("../../../shared/catchAsync"));
-const sendResponse_1 = __importDefault(require("../../../shared/sendResponse"));
-const http_status_1 = __importDefault(require("http-status"));
-const admin_service_1 = require("./admin.service");
-const config_1 = __importDefault(require("../../../config"));
-const createAdmin = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const admin = __rest(req.body, []);
-    const result = yield admin_service_1.AdminService.createAdmin(admin);
-    (0, sendResponse_1.default)(res, {
-        statusCode: http_status_1.default.OK,
-        success: true,
-        message: 'Admin created successfully!',
-        data: result,
-    });
-}));
-const adminLogin = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const loginData = __rest(req.body, []);
-    const result = yield admin_service_1.AdminService.adminLogin(loginData);
-    const { refreshToken } = result, others = __rest(result
-    // set refresh token into cookie
-    , ["refreshToken"]);
+      }
+      function rejected(value) {
+        try {
+          step(generator['throw'](value))
+        } catch (e) {
+          reject(e)
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected)
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next())
+    })
+  }
+var __rest =
+  (this && this.__rest) ||
+  function (s, e) {
+    var t = {}
+    for (var p in s)
+      if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p]
+    if (s != null && typeof Object.getOwnPropertySymbols === 'function')
+      for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+        if (
+          e.indexOf(p[i]) < 0 &&
+          Object.prototype.propertyIsEnumerable.call(s, p[i])
+        )
+          t[p[i]] = s[p[i]]
+      }
+    return t
+  }
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod }
+  }
+Object.defineProperty(exports, '__esModule', { value: true })
+exports.AdminController = void 0
+const catchAsync_1 = __importDefault(require('../../../shared/catchAsync'))
+const sendResponse_1 = __importDefault(require('../../../shared/sendResponse'))
+const http_status_1 = __importDefault(require('http-status'))
+const admin_service_1 = require('./admin.service')
+const config_1 = __importDefault(require('../../../config'))
+const createAdmin = (0, catchAsync_1.default)((req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
+    const admin = __rest(req.body, [])
+    const result = yield admin_service_1.AdminService.createAdmin(admin)
+    ;(0, sendResponse_1.default)(res, {
+      statusCode: http_status_1.default.OK,
+      success: true,
+      message: 'Admin created successfully!',
+      data: result,
+    })
+  })
+)
+const adminLogin = (0, catchAsync_1.default)((req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
+    const loginData = __rest(req.body, [])
+    const result = yield admin_service_1.AdminService.adminLogin(loginData)
+    const { refreshToken } = result,
+      others = __rest(
+        result,
+        // set refresh token into cookie
+        ['refreshToken']
+      )
     // set refresh token into cookie
     const cookieOptions = {
-        secure: config_1.default.env === 'production' ? true : false,
-        httpOnly: true,
-    };
-    res.cookie('refreshToken', refreshToken, cookieOptions);
-    (0, sendResponse_1.default)(res, {
-        statusCode: http_status_1.default.OK,
-        success: true,
-        message: 'admin login successfully!',
-        data: others,
-    });
-}));
-const getAdminProfile = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const requestedUser = __rest(req.user, []);
-    const result = yield admin_service_1.AdminService.getAdminProfile(requestedUser);
-    (0, sendResponse_1.default)(res, {
-        statusCode: http_status_1.default.OK,
-        success: true,
-        message: 'User retrieved successfully !',
-        data: result,
-    });
-}));
-const updateAdminProfile = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const requestedUser = __rest(req.user, []);
-    const updatedData = req.body;
-    const result = yield admin_service_1.AdminService.updateAdminProfile(requestedUser, updatedData);
-    (0, sendResponse_1.default)(res, {
-        statusCode: http_status_1.default.OK,
-        success: true,
-        message: 'User updated successfully !',
-        data: result,
-    });
-}));
+      secure: config_1.default.env === 'production' ? true : false,
+      httpOnly: true,
+    }
+    res.cookie('refreshToken', refreshToken, cookieOptions)
+    ;(0, sendResponse_1.default)(res, {
+      statusCode: http_status_1.default.OK,
+      success: true,
+      message: 'admin login successfully!',
+      data: others,
+    })
+  })
+)
+const getAdminProfile = (0, catchAsync_1.default)((req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
+    const requestedUser = __rest(req.user, [])
+    const result = yield admin_service_1.AdminService.getAdminProfile(
+      requestedUser
+    )
+    ;(0, sendResponse_1.default)(res, {
+      statusCode: http_status_1.default.OK,
+      success: true,
+      message: 'User retrieved successfully !',
+      data: result,
+    })
+  })
+)
+const updateAdminProfile = (0, catchAsync_1.default)((req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
+    const requestedUser = __rest(req.user, [])
+    const updatedData = req.body
+    const result = yield admin_service_1.AdminService.updateAdminProfile(
+      requestedUser,
+      updatedData
+    )
+    ;(0, sendResponse_1.default)(res, {
+      statusCode: http_status_1.default.OK,
+      success: true,
+      message: 'User updated successfully !',
+      data: result,
+    })
+  })
+)
 exports.AdminController = {
-    createAdmin,
-    adminLogin,
-    getAdminProfile,
-    updateAdminProfile,
-};
+  createAdmin,
+  adminLogin,
+  getAdminProfile,
+  updateAdminProfile,
+}
--- dist/app/routes/index.js
+++ dist/app/routes/index.js
@@ -1,21 +1,23 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const user_routes_1 = require("../modules/user/user.routes");
-const cow_routes_1 = require("../modules/cow/cow.routes");
-const order_routes_1 = require("../modules/order/order.routes");
-const admin_route_1 = require("../modules/admin/admin.route");
-const auth_route_1 = require("../modules/auth/auth.route");
-const router = express_1.default.Router();
+'use strict'
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod }
+  }
+Object.defineProperty(exports, '__esModule', { value: true })
+const express_1 = __importDefault(require('express'))
+const user_routes_1 = require('../modules/user/user.routes')
+const cow_routes_1 = require('../modules/cow/cow.routes')
+const order_routes_1 = require('../modules/order/order.routes')
+const admin_route_1 = require('../modules/admin/admin.route')
+const auth_route_1 = require('../modules/auth/auth.route')
+const router = express_1.default.Router()
 const moduleRoutes = [
-    { path: '/users', route: user_routes_1.UserRoutes },
-    { path: '/cows', route: cow_routes_1.CowRoutes },
-    { path: '/orders', route: order_routes_1.OrderRoutes },
-    { path: '/admins', route: admin_route_1.AdminRoutes },
-    { path: '/auth', route: auth_route_1.AuthRoutes },
-];
-moduleRoutes.forEach(route => router.use(route.path, route.route));
-exports.default = router;
+  { path: '/users', route: user_routes_1.UserRoutes },
+  { path: '/cows', route: cow_routes_1.CowRoutes },
+  { path: '/orders', route: order_routes_1.OrderRoutes },
+  { path: '/admins', route: admin_route_1.AdminRoutes },
+  { path: '/auth', route: auth_route_1.AuthRoutes },
+]
+moduleRoutes.forEach(route => router.use(route.path, route.route))
+exports.default = router
